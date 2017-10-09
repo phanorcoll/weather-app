6914b5ea83610b7b221c2fe71229d2a537ccbece
Phanor Coll <phanorcoll@gmail.com>
2017-10-09 15:17:10 -0400

    style to forecast details
    added animations and styles to the weather details

--- app/components/Detail.jsx
+++ app/components/Detail.jsx
@@ -15,12 +15,11 @@ class Detail extends Component {
                 <Day day={props} cStyle='detail' />
                 <div className='info'>
                     <ul>
-                        <li><span>City</span>{props.city}</li>
-                        <li><span>Clouds</span>{props.clouds}</li>
-                        <li><span>Humidity</span>{props.humidity}</li>
-                        <li><span>Pressure</span>{props.pressure}</li>
-                        <li><span>Rain</span>{props.rain}</li>
-                        <li>{props.weather[0].description}</li>
+                        <li><a href='#'>City</a>{props.city}</li>
+                        <li><a href='#'>Humidity</a>{props.humidity}</li>
+                        <li><a href='#'>Max temp</a>{props.temp.max}</li>
+                        <li><a href='#'>Min temp</a>{props.temp.min}</li>
+                        <li><a href='#'>Description</a>{props.weather[0].description}</li>
                     </ul>
                 </div>
             </div>
